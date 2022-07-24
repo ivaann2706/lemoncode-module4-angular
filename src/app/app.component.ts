153b997a9fd6cc491f1d56d6414236840393dc97
Ivan Alba <ivaann2706@gmail.com>
2022-07-24 11:12:40 +0200

    refactor: format the code using Prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lemoncode-module4-angular';
+    title = 'lemoncode-module4-angular';
 }
