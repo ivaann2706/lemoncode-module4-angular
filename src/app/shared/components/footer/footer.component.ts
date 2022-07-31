fd5f9e7075eb6729100403c3e0cd8e44ef6471be
Ivan Alba <ivaann2706@gmail.com>
2022-07-31 15:41:51 +0200

    feat: text in footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-    constructor() {}
+export class FooterComponent {
+    year: number;
 
-    ngOnInit(): void {}
+    constructor() {
+        this.year = new Date().getFullYear();
+    }
 }
