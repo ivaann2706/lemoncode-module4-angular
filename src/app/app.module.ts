34ddf8177e0153137f399594295089bed8db9d1f
Ivan Alba <ivaann2706@gmail.com>
2022-07-31 19:23:51 +0200

    feat: design the navigation bar using Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -29,7 +32,7 @@ import { ProfileComponent } from './profile/profile.component';
         GalleryComponent,
         ProfileComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
